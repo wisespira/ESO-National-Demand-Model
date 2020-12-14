12c078de7ef59cef2aadb006dea6c5c25d9157dd
Ira Watt <irawatt@gmail.com>
2020-12-14 20:22:04 +0000

    Added Week Day correlations

--- dataAnalysis.py
+++ dataAnalysis.py
@@ -8,23 +8,23 @@ import seaborn as sns
 import scipy
 import statsmodels.formula.api as smf
 import statsmodels.api as sm
-## for machine learning
-from sklearn import model_selection, preprocessing, feature_selection, ensemble, linear_model, metrics, decomposition
-## for explainer
-from lime import lime_tabular
-from datetime import datetime
-from sklearn.linear_model import LinearRegression
+
+ESO_Data = pd.read_csv("modelDataSet.csv")
+
+#******************Correlation Testing***********************
+correlationNDtemp = ESO_Data["dailyND"].corr(ESO_Data["London Avg Temp"])
+correlationNDweekDay = ESO_Data["dailyND"].corr(ESO_Data["Week_Day"])
+
+print("Correlation between ND and London Temp " + str(correlationNDtemp))
+print("Correlation between ND and London Weekday " + str(correlationNDweekDay))
+
+print("Week End average demand " +  str(ESO_Data[ESO_Data["Week_Day"]==0].dailyND.mean()))
+print("Week Day average demand " +  str(ESO_Data[ESO_Data["Week_Day"]==1].dailyND.mean()))
 
 
-#******************ESO set-up***********************
-ESO_Data = pd.read_csv("modelData.csv")
-correlation = ESO_Data["dailyND"].corr(ESO_Data["London Avg Temp"])
-
-plt.title('Average Daily National Demand Against \nAvarage Daily Temperature in London')
-sns.regplot(x=ESO_Data['dailyND'],y=ESO_Data['London Avg Temp'])
+#plt.title('Average Daily National Demand Against \nAvarage Daily Temperature in London')
+#sns.regplot(x=ESO_Data['dailyND'],y=ESO_Data['Week_Day'])
 #plt.show()
-print(correlation)
-
 
 
 
@@ -34,17 +34,17 @@ print(correlation)
 
 
 #ESO_Data = ESO_Data.rename(columns={"ND":"Y"})
-def probabilityDistributionGraph(veriable):
-	x = "Y"
+def probabilityDistributionGraph(data):
+	x = "dailyND"
 	fig, ax = plt.subplots(nrows=1, ncols=2,  sharex=False, sharey=False)
 	fig.suptitle(x, fontsize=20)
 	### distribution
 	ax[0].title.set_text('distribution')
-	variable = dtf[x].fillna(dtf[x].mean())
+	variable = data[x].fillna(data[x].mean())
 	breaks = np.quantile(variable, q=np.linspace(0, 1, 11))
 	variable = variable[ (variable > breaks[0]) & (variable < breaks[10]) ]
 	sns.distplot(variable, hist=True, kde=True, kde_kws={"shade": True}, ax=ax[0])
-	des = dtf[x].describe()
+	des = data[x].describe()
 	ax[0].axvline(des["25%"], ls='--')
 	ax[0].axvline(des["mean"], ls='--')
 	ax[0].axvline(des["75%"], ls='--')
@@ -55,14 +55,14 @@ def probabilityDistributionGraph(veriable):
 
 	### boxplot 
 	ax[1].title.set_text('outliers (log scale)')
-	tmp_dtf = pd.DataFrame(dtf[x])
+	tmp_dtf = pd.DataFrame(data[x])
 	tmp_dtf[x] = np.log(tmp_dtf[x])
 	tmp_dtf.boxplot(column=x, ax=ax[1])
 	plt.show()
 
 
 
-
+#probabilityDistributionGraph(ESO_Data)
 
 
 
